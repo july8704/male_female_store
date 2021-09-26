3c163dd2bcd1e6978b7ff41aa98f53795fd92284
july8704 <ingcorrea87@hotmail.com>
2021-09-26 15:47:44 -0500

    The code have been reorganized in functions into a file call preparation.py

--- importing_proveedor.py
+++
@@ -1,5 +0,0 @@
-import numpy as np
-import pandas as pd
-
-pd_proveedores = pd.read_excel ("Data/Proveedores.xlsx")
-pd_proveedores.to_csv("Data/Proveedores.csv", sep = ";", index= True, index_label= "Cons")
